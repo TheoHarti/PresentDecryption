6edb516bcf79491e16ec55e556221c680af2ad6a
Theodor Hartleitner <theo.hartleitner@gmail.com>
2023-12-23 18:45:54 +0100

    finale version

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-function decrypt() {
-    const shift = 3; // Your chosen shift number
-    let encryptedName = document.getElementById('encryptedName').value.toUpperCase();
-    let decryptedName = '';
+function encrypt() {
+    processText(true);
+}
 
-    for (let i = 0; i < encryptedName.length; i++) {
-        if (encryptedName[i] != ' ') {
-            let charCode = encryptedName.charCodeAt(i) - shift;
-            if (charCode < 65) charCode += 26;
-            decryptedName += String.fromCharCode(charCode);
-        } else {
-            decryptedName += ' ';
-        }
+function decrypt() {
+    processText(false);
+}
+
+function processText(isEncrypting) {
+    let key = document.getElementById('key').value.toUpperCase().replace(/[^A-Z]/g, '');
+    let inputText = document.getElementById('inputText').value.toUpperCase().replace(/[^A-Z]/g, '');
+    let outputText = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < inputText.length; i++) {
+        let shift = key.charCodeAt(keyIndex % key.length) - 65;
+        let charCode = inputText.charCodeAt(i) + (isEncrypting ? shift : -shift);
+        if (charCode > 90) charCode -= 26;
+        if (charCode < 65) charCode += 26;
+        outputText += String.fromCharCode(charCode);
+
+        keyIndex++;
     }
 
-    document.getElementById('result').innerText = 'Entschlüsselter Name: ' + decryptedName;
+    document.getElementById('result').innerText = isEncrypting ? 'Encrypted: ' + outputText : 'Decrypted: ' + outputText;
 }
