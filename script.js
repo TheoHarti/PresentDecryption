6af49a48778e8f7dddc3c48ef5d98e46f1cff373
Theodor Hartleitner <theo.hartleitner@gmail.com>
2023-12-24 09:11:27 +0100

    added riddles

--- script.js
+++ script.js
@@ -24,3 +24,49 @@ function processText(isEncrypting) {
 
     document.getElementById('result').innerText = isEncrypting ? 'Encrypted: ' + outputText : 'Decrypted: ' + outputText;
 }
+
+function checkAnswer(step, correctAnswer) {
+    var userAnswer = document.getElementById('answer' + step).value.toLowerCase().trim();
+    if (userAnswer === correctAnswer) {
+        if (step === 4) {
+            document.querySelectorAll('.page').forEach(page => page.style.display = 'none');
+            document.getElementById('finalPage').style.display = 'block';
+        } else {
+            goToNextStep(step + 1);
+        }
+        
+        if (step === 3) {
+            startTimer()
+        }
+
+    } else {
+        alert('Falsche Antwort, versuche es nochmal!');
+    }
+}
+
+function goToNextStep(step) {
+    document.querySelectorAll('.page').forEach(page => page.style.display = 'none');
+    document.getElementById('riddlePage' + step).style.display = 'block';
+}
+
+function startTimer() {
+    var duration = 180; // 3 minutes in seconds
+    var timer = duration, minutes, seconds;
+    var interval = setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        document.getElementById('timer').textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            clearInterval(interval);
+            document.getElementById('timer').textContent = 'Zeit abgelaufen!';
+            document.getElementById('nextButton').disabled = true;
+        }
+    }, 1000);
+
+    document.getElementById('riddlePage4').style.display = 'block';
+}
